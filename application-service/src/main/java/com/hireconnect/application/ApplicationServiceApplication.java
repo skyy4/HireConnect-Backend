1c69c548c07de67add83ec40e0da7870c52ebca0
skyy4 <skyonly4code@gmail.com>
2026-04-21 14:43:07 +0530

    feat: implement application-service

---
+++ application-service/src/main/java/com/hireconnect/application/ApplicationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hireconnect.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationServiceApplication.class, args);
+    }
+}
